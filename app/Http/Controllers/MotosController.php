58dc432ac3f081873d5cfcbe5231421509df2702
Henry-Lima <henry.lima.teco@gmail.com>
2025-03-14 22:36:49 -0300

    First commit

---
+++ app/Http/Controllers/MotosController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Motos;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Response;
+
+class MotosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $registroMoto = Motos::All();
+
+        $contador = $registrosMoto->count();
+
+        return('Motos Listadas  : '.$contador.$registrosMoto.Response()->json([],Response::HTTP_NO_CONTENT));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $registrosMoto = $request->All();
+
+        $validaDados = Validator::make($registrosMoto,[
+            'modelo' => 'required',
+            'marca' => 'required',
+            'ano' => 'required',
+        ]);
+
+        if($validaDados->fails()) {
+            return 'Registros faltantes: '.Response()->json([],Response::HTTP_NO_CONTENT);
+
+        }            
+            $enviaDados = Motos::create($registrosMoto);
+
+            if($enviaDados) {
+                return 'Registros Cadastrados: '.Response()->json([],Response::HTTP_NO_CONTENT);
+            }else {
+                return 'Registros não cadastrados: '.Response()->json([],Response::HTTP_NO_CONTENT);
+            }
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Motos $motos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Motos $motos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Motos $motos)
+    {
+        //
+    }
+}
---
+++ app/Models/Motos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Motos extends Model
+{
+    protected $fillable = [
+        'modelo',
+        'marca',
+        'ano',
+    ];
+}
